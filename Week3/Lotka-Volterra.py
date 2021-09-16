6384126670d76931c767d4f803c650e5bee8c2b1
Jishnu Srivastava <82699906+Jks08@users.noreply.github.com>
2021-09-16 10:51:36 +0530

    Create Lotka-Volterra.py
    This is my solution to the project Lotka-Volterra.

---
+++ Week3/Lotka-Volterra.py
@@ -0,0 +1,6 @@
+#For the following project, on Windows, first install on setup MikTex and then run the code in your command prompt. 
+#This will display a bunch of images each corresponding to a set of Array values. Infer the required output from that.
+
+import numpy as np
+import math
+import matplotlib.pyplot as plt
